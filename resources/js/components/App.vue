447f907245e982f99f9bd5b1b96cc1cd9f733704
Максим Лисянский <max_lisyanskiy@mail.ru>
2021-07-12 23:51:04 +0300

    install vue-router

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="container">
-        <h1>Hello</h1>
+        <div>
+            <router-link :to="{name: 'home'}">Home</router-link>
+            <router-link :to="{name: 'about'}">About</router-link>
+        </div>
+        <div>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "App.vue"
+
     }
 </script>
 
